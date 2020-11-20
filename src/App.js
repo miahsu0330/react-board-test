5cb3a827eaa6f25c44a7abc75b99b5999ba76eb5
miahsu <Mia.hsu@thinkpower-info.com>
2020-11-20 21:16:00 +0800

    test

--- src/App.js
+++ src/App.js
@@ -1,23 +1,131 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import styled from "styled-components";
+import { useState, useRef } from "react";
+
+const TodoItemWrapper = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  paddiig: 8px 16px;
+  border: 1px solid black;
+
+  & + & {
+    margin-top: 12px;
+  }
+`;
+
+const TodoConent = styled.div`
+  color: #333;
+  font-size: 12px;
+
+  ${(props) =>
+    props.size === "XL" &&
+    `
+    font-size: 20px
+  `}
+
+  ${(props) =>
+    props.isDone &&
+    `
+    text-decoration: line-through;
+  `}
+`;
+
+const TodoButtonWrapper = styled.div``;
+
+const Button = styled.button`
+  padding: 4px;
+  color: black;
+`;
+// 加 $ stylecompone
+
+function TodoItem({ size, todo, handleDeleteTodo, handleToggleIsDone }) {
+  return (
+    <TodoItemWrapper data-todo-id={todo.id}>
+      <TodoConent size={size}>{todo.content}</TodoConent>
+      <TodoButtonWrapper>
+        <Button
+          onClick={() => {
+            handleToggleIsDone(todo.id);
+          }}
+        >
+          {todo.isDone ? "未完成" : "已完成"}
+        </Button>
+        <Button
+          onClick={() => {
+            handleDeleteTodo(todo.id);
+          }}
+        >
+          刪除
+        </Button>
+      </TodoButtonWrapper>
+    </TodoItemWrapper>
+  );
+}
+
+// hook function component
+// 解構語法
+
+let id = 2;
 
 function App() {
+  const [todos, setTodos] = useState([
+    { id: 1, content: "abc", isDone: true },
+    { id: 2, content: "adddbc", isDone: false },
+  ]);
+
+  const [value, setValue] = useState("");
+
+  function handleButtonClick() {
+    setTodos([
+      {
+        id,
+        content: value,
+      },
+      ...todos,
+    ]);
+    setValue("");
+    id++;
+  }
+
+  const handleInputChange = (e) => {
+    setValue(e.target.value);
+  };
+
+  const handleToggleIsDone = (id) => {
+    setTodos(
+      todos.map((todo) => {
+        if (todo.id !== id) return todo;
+        return {
+          ...todo,
+          isDone: !todo.isDone,
+        };
+      })
+    );
+  };
+
+  const handleDeleteTodo = (id) => {
+    // 篩選出不等於要刪除的 id
+    setTodos(todos.filter((todo) => todo.id !== id));
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <input
+        type="text"
+        placeholder="todo"
+        value={value}
+        onChange={handleInputChange}
+      />
+      <button onClick={handleButtonClick}>increment</button>
+      {todos.map((todo, index) => (
+        <TodoItem
+          key={todo.id}
+          todo={todo}
+          handleDeleteTodo={handleDeleteTodo}
+          handleToggleIsDone={handleToggleIsDone}
+        />
+      ))}
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,14 +1,13 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals"; // 網站效能有關
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  // 嚴格模式 React.StrictMode
+  <App />,
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
